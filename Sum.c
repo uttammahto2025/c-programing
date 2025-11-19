131df67eec94287fc2b09564abfba89b38cd486c
uttam829 <uttamma72@gmail.com>
2025-11-20 00:13:29 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, Sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    Sum = a + b;
+    printf("Adition of the given integers : %d\n", Sum);
+    return 0;
+    
+}
